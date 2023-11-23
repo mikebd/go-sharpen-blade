efa09f3bc6e4d56fbf5b6e9460e16a1e5da102aa
Michael Ben-David <mikebd@users.noreply.github.com>
2023-11-23 04:51:00 -0500

    currentBranchName() and isBranchOfInterest()

--- playground/gitPull/include.go
+++ playground/gitPull/include.go
@@ -2,7 +2,10 @@ package gitPull
 
 import (
 	"os"
+	"os/exec"
 )
+
+var branchesOfInterest = []string{"main", "master"}
 
 func includeGitRepositoryDirectory(directory string) bool {
 	currentDir, err := os.Getwd()
@@ -17,5 +20,28 @@ func includeGitRepositoryDirectory(directory string) bool {
 		_ = os.Chdir(dir)
 	}(currentDir)
 
+	branch := currentBranchName()
+
+	if !isBranchOfInterest(branch) {
+		return false
+	}
 	return true
+}
+
+func currentBranchName() string {
+	cmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
+	output, err := cmd.Output()
+	if err != nil || len(output) <= 1 {
+		return ""
+	}
+	return string(output)[:len(output)-1]
+}
+
+func isBranchOfInterest(branch string) bool {
+	for _, branchOfInterest := range branchesOfInterest {
+		if branch == branchOfInterest {
+			return true
+		}
+	}
+	return false
 }
