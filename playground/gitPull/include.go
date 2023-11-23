c0b260e652f1417a1c8fdc25f52e88b588118be4
Michael Ben-David <mikebd@users.noreply.github.com>
2023-11-23 05:23:56 -0500

    isBehindRemote()

--- playground/gitPull/include.go
+++ playground/gitPull/include.go
@@ -3,9 +3,14 @@ package gitPull
 import (
 	"os"
 	"os/exec"
+	"strconv"
 )
 
 var branchesOfInterest = []string{"main", "master"}
+
+// remote is the repository short name that we want to track.
+// This might become configurable in the future.
+const remote = "origin"
 
 func includeGitRepositoryDirectory(directory string) bool {
 	currentDir, err := os.Getwd()
@@ -25,6 +30,11 @@ func includeGitRepositoryDirectory(directory string) bool {
 	if !isBranchOfInterest(branch) {
 		return false
 	}
+
+	if !isBehindRemote(branch) {
+		return false
+	}
+
 	return true
 }
 
@@ -44,4 +54,34 @@ func isBranchOfInterest(branch string) bool {
 		}
 	}
 	return false
+}
+
+func isBehindRemote(branch string) bool {
+	fetchErr := fetch(branch)
+	if fetchErr != nil {
+		return false
+	}
+
+	cmd := exec.Command("git", "rev-list", "--count", branch+".."+remote+"/"+branch)
+	output, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+	return isCommandOutputGreaterThanZero(output)
+}
+
+func fetch(branch string) error {
+	cmd := exec.Command("git", "fetch", remote, branch)
+	return cmd.Run()
+}
+
+func isCommandOutputGreaterThanZero(output []byte) bool {
+	if len(output) <= 1 {
+		return false
+	}
+	value, err := strconv.Atoi(string(output)[:len(output)-1])
+	if err != nil {
+
+	}
+	return value > 0
 }
