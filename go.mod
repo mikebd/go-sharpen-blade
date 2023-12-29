f2e83c311ebea0a3c3680ec0091066c85f0a7151
Michael Ben-David <mikebd@users.noreply.github.com>
2023-12-29 12:51:35 -0500

    sudoku: passing Test_board_validate_invalid
    invalid row - duplicate value

--- go.mod
+++ go.mod
@@ -3,4 +3,7 @@ module go-sharpen-blade
 go 1.21
 
 require github.com/mikebd/go-util v0.0.5
+
 require github.com/samber/lo v1.39.0
+
+require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
--- playground/sudoku/board/validate.go
+++ playground/sudoku/board/validate.go
@@ -1,6 +1,7 @@
 package board
 
 import (
+	"github.com/samber/lo"
 	lop "github.com/samber/lo/parallel"
 )
 
@@ -17,18 +18,18 @@ func (board *board) validate() invalid {
 
 	type args struct {
 		cells       cellPointersArray
-		resultField []int
+		resultField *[]int
 	}
 
 	lop.ForEach([]args{
-		{board.rows, result.rows},
-		{board.cols, result.cols},
-		{board.scts, result.scts},
+		{board.rows, &result.rows},
+		{board.cols, &result.cols},
+		{board.scts, &result.scts},
 	},
 		func(args args, _ int) {
-			lop.ForEach([]cellPointersArray{args.cells}, func(cells cellPointersArray, col int) {
-				if invalidCells(cells) {
-					args.resultField = append(args.resultField, col)
+			lop.ForEach([]cellPointersArray{args.cells}, func(cells cellPointersArray, index int) {
+				if invalidCells(cells[index]) {
+					*args.resultField = append(*args.resultField, index)
 				}
 			})
 		})
@@ -36,6 +37,24 @@ func (board *board) validate() invalid {
 	return result
 }
 
-func invalidCells(cells cellPointersArray) bool {
-	return true
+func invalidCells(cells cellPointers) bool {
+	cellValue := func(cell *cell) value {
+		if cell == nil {
+			return empty
+		}
+		return cell.value
+	}
+
+	// duplicate values
+	// count the number of times each value appears
+	// if any value appears more than once, the cells are invalid
+	counts := lo.CountValuesBy(cells, cellValue)
+
+	for value, count := range counts {
+		if value != empty && count > 1 {
+			return true
+		}
+	}
+
+	return false
 }
