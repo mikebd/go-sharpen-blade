18d3408a1101377d9258528652496b967f46b433
Michael Ben-David <mikebd@users.noreply.github.com>
2024-01-01 14:40:52 -0500

    sudoku: extract external representation of cell values

--- playground/sudoku/board/create.go
+++ playground/sudoku/board/create.go
@@ -1,18 +1,6 @@
 package board
 
-const (
-	emptyInitialValue rune = ' '
-	oneInitialValue   rune = '1'
-	twoInitialValue   rune = '2'
-	threeInitialValue rune = '3'
-	fourInitialValue  rune = '4'
-	fiveInitialValue  rune = '5'
-	sixInitialValue   rune = '6'
-	sevenInitialValue rune = '7'
-	eightInitialValue rune = '8'
-	nineInitialValue  rune = '9'
-)
-
+// create creates a board with the given initial values.
 func create(initialValues [size]string) Board {
 	board := Board{}
 
