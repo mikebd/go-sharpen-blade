9cd1247cbd32d04c0f6c4f0fd963a6d5c437162d
Michael Ben-David <mikebd@users.noreply.github.com>
2024-01-24 20:15:39 -0500

    sudoku: fix compile

--- playground/sudoku/game/game.go
+++ playground/sudoku/game/game.go
@@ -3,10 +3,12 @@ package game
 import (
 	"go-sharpen-blade/command"
 	. "go-sharpen-blade/playground/sudoku/board"
+	"go-sharpen-blade/playground/sudoku/game/port"
 )
 
 type game struct {
 	board *Board
+	turns []*port.Turn
 }
 
 func Register() {
