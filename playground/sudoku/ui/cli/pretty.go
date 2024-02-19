d04e554591d24257419ae9e32a2a0627e62c9548
Michael Ben-David <mikebd@users.noreply.github.com>
2024-02-18 20:50:37 -0500

    sudoku: wip pretty render

--- playground/sudoku/ui/cli/pretty.go
+++ playground/sudoku/ui/cli/pretty.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 	"go-sharpen-blade/playground/sudoku/game/port"
 	"os"
 )
@@ -9,7 +10,19 @@ import (
 func renderBoard(turns []*port.Turn) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
-	t.AppendHeader(table.Row{"#", "Row", "Column", "Value", "Valid"})
+	t.SetStyle(table.StyleColoredBlackOnBlueWhite)
+	t.Style().Color.IndexColumn = text.Colors{text.BgBlue, text.FgHiWhite}
+	t.AppendHeader(table.Row{"#", "A", "B", "C", "D", "E", "F", "G", "H", "I"})
+	t.AppendRow(table.Row{"1", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"2", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"3", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"4", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"5", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"6", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"7", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"8", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+	t.AppendRow(table.Row{"9", "1", "2", "3", "4", "5", "6", "7", "8", "9"})
+
 	/*	for i, turn := range turns {
 			t.AppendRow(table.Row{i, turn.Row(), turn.Col(), turn.Value(), turn.Valid()})
 		}
