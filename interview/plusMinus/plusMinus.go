0896e6fdf2ee7ca7e25d0030f0f026303e2b257e
Michael Ben-David <mikebd@users.noreply.github.com>
2024-03-22 18:00:33 -0400

    interview: plusMinus handle all decimal digits

--- interview/plusMinus/plusMinus.go
+++ interview/plusMinus/plusMinus.go
@@ -7,16 +7,49 @@ func plusMinus(expr string) int {
 	operator := '+'
 
 	for i := 0; i < len(expr); i++ {
-		switch {
-		case expr[i] == 'o' || expr[i] == 'O':
+		switch expr[i] {
+		case 'o', 'O':
 			result = operate(result, operator, 1)
 			i += 2
-		case expr[i] == 't' || expr[i] == 'T':
-			result = operate(result, operator, 2)
-			i += 2
-		case expr[i] == '+':
+		case 'e', 'E':
+			result = operate(result, operator, 8)
+			i += 4
+		case 'f', 'F':
+			switch expr[i+1] {
+			case 'i', 'I':
+				result = operate(result, operator, 5)
+				i += 3
+			case 'o', 'O':
+				result = operate(result, operator, 4)
+				i += 3
+			}
+		case 'n', 'N':
+			result = operate(result, operator, 9)
+			i += 3
+		case 's', 'S':
+			switch expr[i+1] {
+			case 'i', 'I':
+				result = operate(result, operator, 6)
+				i += 2
+			case 'e', 'E':
+				result = operate(result, operator, 7)
+				i += 4
+			}
+		case 't', 'T':
+			switch expr[i+1] {
+			case 'w', 'W':
+				result = operate(result, operator, 2)
+				i += 2
+			case 'h', 'H':
+				result = operate(result, operator, 3)
+				i += 4
+			}
+		case 'z', 'Z':
+			result = operate(result, operator, 0)
+			i += 3
+		case '+':
 			operator = '+'
-		case expr[i] == '-':
+		case '-':
 			operator = '-'
 		}
 	}
--- interview/plusMinus/plusMinus_test.go
+++ interview/plusMinus/plusMinus_test.go
@@ -12,10 +12,23 @@ func TestPlusMinus(t *testing.T) {
 		want int
 	}{
 		{"one+two", args{"one+two"}, 3},
+		{
+			"one+two+three+four+five+six+seven+eight+nine+zero",
+			args{"one+two+three+four+five+six+seven+eight+nine+zero"},
+			45},
 		{"one+two-one-one+two+one", args{"one+two-one-one+two+one"}, 4},
 		{"two-two-one-two", args{"two-two-one-two"}, -3},
-		{"two", args{"two"}, 2},
 		{"one-two", args{"one-two"}, -1},
+		{"one", args{"one"}, 1},
+		{"two", args{"two"}, 2},
+		{"three", args{"three"}, 3},
+		{"four", args{"four"}, 4},
+		{"five", args{"five"}, 5},
+		{"six", args{"six"}, 6},
+		{"seven", args{"seven"}, 7},
+		{"eight", args{"eight"}, 8},
+		{"nine", args{"nine"}, 9},
+		{"zero", args{"zero"}, 0},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
